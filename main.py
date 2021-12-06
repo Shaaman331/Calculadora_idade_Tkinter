284ba0212494d2ce06de8b06072438ae80bc3f46
Shaaman331 <tarcisio2010souza@gmail.com>
2021-12-06 19:22:01 -0300

    Criando frames e cores.

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ janela = Tk()
 janela.title('Calculadora de idade')
 janela.geometry('310x400')
 
+# cores
+cor1= '#3b3b3b'
+cor2= '#333333'
+
+
+# criando frames
+frame_cima = Frame(janela, width=310, height=140, pady=0, padx=0, relief=FLAT, bg=cor2) #Largura, altura, estilo Flat, cor 
+frame_cima.grid(row=0, column=0)
+
+frame_baixo = Frame(janela, width=310, height=300, pady=0, padx=0, relief=FLAT, bg=cor1) #Largura, altura, estilo Flat, cor 
+frame_baixo.grid(row=1, column=0)
 
 janela.mainloop()
